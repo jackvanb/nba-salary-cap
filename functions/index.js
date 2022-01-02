f112becd899e7bee6e5274a8b755c8e23af104a2
Jack Van Boening <jackvanboening@gmail.com>
2022-01-01 17:04:26 -0800

    create new function route to get player data from a team.

--- functions/index.js
+++ functions/index.js
@@ -39,3 +39,48 @@ exports.capSpace = functions.https.onCall(async (data, context) => {
     return [];
   }
 });
+
+const cachedTeamCapSpace = {};
+
+exports.teamCapSpace = functions.https.onCall(async (data, context) => {
+  // Format the team name to the url path for Sportrac request.
+  // i.e Golden State Warriors -> golden-state-warriors
+  const teamUrlPath = data.teamName.replace(/\s+/g, "-").toLowerCase();
+  const sportTracUrl = `https://www.spotrac.com/nba/${teamUrlPath}/cap/`;
+
+  if (cachedTeamCapSpace[teamUrlPath] != null) {
+    return cachedTeamCapSpace[teamUrlPath];
+  }
+  try {
+    const response = await fetch(sportTracUrl);
+    if (!response.ok) {
+      console.error(response.statusText);
+      return [];
+    }
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const teamCapSpace = [];
+    // Loop through the first table only.
+    const firstTable = $("body").find("table")[0];
+    const cheerioFirstTable = $(firstTable).find("tbody > tr");
+
+    cheerioFirstTable.each((index, element) => {
+      const row = $(element).find("td");
+      const playerName = $(row[0]).find("a").text();
+      const salary = $(row[3]).text().trim();
+      const capFigure = $(row[7]).find("span").text().trim();
+      const capPercentage = $(row[8]).text().trim();
+      teamCapSpace.push({
+        playerName: playerName,
+        salary: salary,
+        capFigure: capFigure,
+        capPercentage: capPercentage,
+      });
+    });
+    cachedTeamCapSpace[teamUrlPath] = teamCapSpace;
+    return teamCapSpace;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+});
